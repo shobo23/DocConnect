abe8ba1fa1001a2e3e8cdb3dab5d1f927a61de26
Shobo Muhammed <shobomuhammed23@gmai.com>
2024-12-29 08:27:06 +0930

    Added login and signUp page with responsiveness

--- script.js
+++ script.js
@@ -1,8 +1,14 @@
-const navBar = document.getElementById("nav-bar");
-const authButton = document.getElementById("auth-btn");
-const burgerMenu = document.getElementById("burger_menu");
+// const navBar = document.getElementById("nav-bar");
+// const authButton = document.getElementById("auth-btn");
+// const burgerMenu = document.getElementById("burger_menu");
 
-burgerMenu.addEventListener("click", ()=>
-    navBar.classList.toggle("show"),
-    authButton.classList.toggle("show")
-)
+// burgerMenu.addEventListener("click", ()=>
+//     navBar.classList.toggle("show"),
+//     authButton.classList.toggle("show")
+// )
+const burgerMenu = document.getElementById("burger_menu");
+  const navBar = document.getElementById("nav-bar");
+
+  burgerMenu.addEventListener("click", () => {
+    navBar.classList.toggle("active");
+  });
